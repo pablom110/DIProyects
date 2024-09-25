5869689e0f3b027efbdfc32eb25460daaef36cc6
pablom110 <pablomfe98@educastur.es>
2024-09-25 10:42:43 +0200

    Agregar archivos de proyecto.

---
+++ PagesDemo/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace PagesDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ContentPageDemo());
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+    }
+
+}
